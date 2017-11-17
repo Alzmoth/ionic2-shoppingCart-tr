505097748771ba4f14e3043f78891e81fe341879
alzmoth <malinayir@gmail.com>
2017-11-17 15:31:25 +0300

    stok sayımda excel coklu sutuna cevirilecek

--- src/pages/liste-goster/liste-goster.ts
+++ src/pages/liste-goster/liste-goster.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 import * as papa from 'papaparse';
 import { Http } from '@angular/http'
-import {Headers} from '../../entities/excel'
+import { Headers } from '../../entities/excel'
 import { parseDate } from 'ionic-angular/util/datetime-util';
+import { Console } from '@angular/core/src/console';
+import { textDef } from '@angular/core/src/view/text';
+import { Text } from '@angular/compiler';
+import { MomentModule } from 'angular2-moment';
+
+import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
 
 /**
  * Generated class for the ListeGosterPage page.
@@ -21,52 +29,44 @@ export class ListeGosterPage {
 
   gelen: any;
   public dataSet: any[] = [];
-  headerRow: any[] = [];
-  csvData: any[] = [];
-  
+ 
+  saveData: any[] = [];
+  public splitPaneState: boolean;
+
 
   constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    
-   ) {
-    this.dataSet=navParams.get('item');
-    this.gelen=navParams.get('item2');
-    
+    public navParams: NavParams, private http: Http,
+    public platform: Platform,
+    public menu: MenuController
+
+
+  ) {
+    this.splitPaneState = false;
+
+    this.dataSet = navParams.get('item');
+    this.gelen = navParams.get('item2');
+    console.log("hello", this.gelen)
+    this.menu.enable(false);
+    setTimeout(() => this.navCtrl.pop(), 7000);
+    setTimeout(() => this.menu.enable(true), 7000)
+
+
+    this.dataSet.forEach(element => {
+      delete element.stok_id;
+      delete element.fatura_no;
+    });
+    console.log("ilk hali", this.dataSet)
+    // this.extractData();
   }
+
+
+
+ 
 
   ionViewDidLoad() {
-    console.log(this.dataSet);
-   // console.log(this.gelen);
-  
-  
-  }
-  downloadCSV(){
-    
-    let csv=papa.unparse({
-      fields:["Stok Adı","Stok Kodu","Stok Adedi","Barkod"],
-       data:["deliveli"]
-      
-        });
-    console.log(csv)
 
-    var blob = new Blob([csv]);
-    var a = window.document.createElement("a");
-    document.getElementById("a");
-    a.href = window.URL.createObjectURL(blob);
-    a.charset = "iso-8859-9"
-    a.download="newdata.csv";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
-  handleError(err){
 
   }
 
-
-
-  trackByFn(index:any,item:any){
-    return index;
-  }
 
 }
